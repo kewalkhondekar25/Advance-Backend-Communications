ea976c862400a29ae06209b61371c10fa6fb572b
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-14 15:33:31 +0530

    feat: singleton pattern added

--- stateFull-backend/src/store.ts
+++ stateFull-backend/src/store.ts
@@ -16,9 +16,21 @@ export class GameStore {
 
   games: ChessGame[] = [];
 
-  constructor(){
+  //singleton pattern
+  private static instance: GameStore;// Holds the single instance
+  private constructor(){// Private constructor prevents direct instantiation
     this.games = [];
   };
+
+  static getInstance(){
+    //it creates a single instance of game manager & returns it from inside
+    // const store = new GameStore();
+    if(GameStore.instance){
+      return GameStore.instance
+    };
+    GameStore.instance = new GameStore();
+    return GameStore.instance
+  }
 
   addMove(id: string, whiteArmyPlayerName: string, blackArmyPlayerName: string, whiteMoves: string, blackMoves: string){
     this.games.push({ id, whiteArmyPlayerName, blackArmyPlayerName, moves: [{ whiteMoves, blackMoves}]})
@@ -28,5 +40,8 @@ export class GameStore {
     console.log(JSON.stringify(this.games, null, 2));
   };
 };
+
+//singleton
+export const gameStore = GameStore.getInstance();
 
 export const store = new GameStore();
